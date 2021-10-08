b01026a3f12ccdb22ca3f39829ad697aef25243b
Vidhan Bhatt <me@vidhan.io>
2021-10-08 18:42:40 -0400

    Update to add initialize and verify

--- go.mod
+++ go.mod
@@ -6,5 +6,5 @@ require (
 	github.com/bwmarrin/discordgo v0.23.3-0.20210821175000-0fad116c6c2a
 	github.com/gorilla/websocket v1.4.2 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
-	golang.org/x/sys v0.0.0-20211006194710-c8a6f5223071 // indirect
+	golang.org/x/sys v0.0.0-20211007075335-d3039528d8ac // indirect
 )
--- main.go
+++ main.go
@@ -1,20 +1,43 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/signal"
+	"strings"
 
 	"github.com/bwmarrin/discordgo"
 )
 
+type Students struct {
+	Students [26][26][]Student `json:"students"`
+}
+
+type Student struct {
+	Initials       [2]string `json:"initials"`
+	Grade          int       `json:"grade"`
+	TeacherInitial string    `json:"teacher_initial"`
+	StudentNumber  int       `json:"student_number"`
+}
+
+type Guilds struct {
+	Guilds []Guild `json:"guilds"`
+}
+
+type Guild struct {
+	ID           string `json:"id"`
+	VerifiedRole string `json:"verified_role"`
+}
+
 // Bot parameters
 var (
-	GuildID        = flag.String("guild", "", "Test guild ID. If not passed - bot registers commands globally")
-	BotToken       = flag.String("token", "", "Bot access token")
-	RemoveCommands = flag.Bool("rmcmd", true, "Remove all commands after shutdowning or not")
+	GuildID  = flag.String("guild", "", "Test guild ID. If not passed - bot registers commands globally")
+	BotToken = flag.String("token", "", "Bot access token")
 )
 
 var s *discordgo.Session
@@ -32,54 +55,128 @@ func init() {
 var (
 	commands = []*discordgo.ApplicationCommand{
 		{
-			Name:        "authenticate",
-			Description: "Authenticate yourself for access to the server.",
+			Name:        "verify",
+			Description: "Verify yourself for access to the server.",
 			Options: []*discordgo.ApplicationCommandOption{
 
 				{
 					Type:        discordgo.ApplicationCommandOptionString,
-					Name:        "first_name",
+					Name:        "first-name",
 					Description: "Your first name.",
 					Required:    true,
 				},
 				{
 					Type:        discordgo.ApplicationCommandOptionString,
-					Name:        "last_name",
+					Name:        "last-name",
 					Description: "Your last name.",
 					Required:    true,
 				},
 				{
 					Type:        discordgo.ApplicationCommandOptionInteger,
-					Name:        "student_number",
-					Description: "Your student number (6 digits).",
+					Name:        "grade",
+					Description: "Your grade.",
 					Required:    true,
 				},
 				{
 					Type:        discordgo.ApplicationCommandOptionString,
-					Name:        "homeroom_teacher_last_name",
+					Name:        "teacher-name",
 					Description: "The last name of your homeroom teacher (Week 1, Period 1)",
+					Required:    true,
+				},
+				{
+					Type:        discordgo.ApplicationCommandOptionInteger,
+					Name:        "student-number",
+					Description: "Your student number (6 digits).",
+					Required:    true,
+				},
+			},
+		},
+		{
+			Name:        "initialize",
+			Description: "Initialize the server with Woodlands Checkpoint.",
+			Options: []*discordgo.ApplicationCommandOption{
+
+				{
+					Type:        discordgo.ApplicationCommandOptionRole,
+					Name:        "verified-role",
+					Description: "The role to give to verified users.",
 					Required:    true,
 				},
 			},
 		},
 	}
 	commandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
-		"authenticate": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-			margs := []interface{}{
-				i.ApplicationCommandData().Options[0].StringValue(),
-				i.ApplicationCommandData().Options[1].StringValue(),
-				i.ApplicationCommandData().Options[2].IntValue(),
-				i.ApplicationCommandData().Options[3].StringValue(),
+		"verify": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+
+			firstName := strings.Title(i.ApplicationCommandData().Options[0].StringValue())
+			lastName := strings.Title(i.ApplicationCommandData().Options[1].StringValue())
+			grade := i.ApplicationCommandData().Options[2].IntValue()
+			teacherName := i.ApplicationCommandData().Options[3].StringValue()
+			studentNumber := i.ApplicationCommandData().Options[4].IntValue()
+
+			student := NewStudent(firstName, lastName, int(grade), teacherName, int(studentNumber))
+
+			studentVerification, err := verifyStudent(student, students)
+
+			var msg string
+
+			if err != nil {
+				msg = "Error: " + err.Error()
+			} else {
+				if studentVerification {
+					var currentGuild Guild
+					for _, guild := range guilds.Guilds {
+						if guild.ID == i.GuildID {
+							currentGuild = guild
+						}
+					}
+					msg = "You are verified! Welcome!"
+					if len(currentGuild.ID) > 0 {
+						_ = s.GuildMemberRoleAdd(currentGuild.ID, i.Member.User.ID, currentGuild.VerifiedRole)
+					}
+
+				} else {
+					msg = "Sorry, your infromation is invalid."
+				}
 			}
-			msgformat := "Hello %s %s! Your email address is %d@pdsb.net, and your homeroom teacher is %s!"
+
 			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 				Type: discordgo.InteractionResponseChannelMessageWithSource,
 				Data: &discordgo.InteractionResponseData{
-					Content: fmt.Sprintf(
-						msgformat,
-						margs...,
-					),
-					Flags: 1 << 6,
+					Content: msg,
+					Flags:   1 << 6,
+				},
+			})
+		},
+		"initialize": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+
+			roleID := i.ApplicationCommandData().Options[0].RoleValue(s, "").ID
+			guildID := i.GuildID
+
+			guildTemp, _ := s.Guild(i.GuildID)
+
+			var msg string
+
+			if i.Member.User.ID == guildTemp.OwnerID {
+				guild := Guild{
+					ID:           guildID,
+					VerifiedRole: roleID,
+				}
+				guilds.Guilds = append(guilds.Guilds, guild)
+				msg = fmt.Sprintf("Set role to <@&%s>", roleID)
+
+				file, _ := json.Marshal(guilds)
+
+				_ = ioutil.WriteFile("guilds.json", file, 0644)
+			} else {
+				msg = "You do not have sufficient permissions. You must be the owner of the guild."
+			}
+
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: msg,
+					Flags:   1 << 6,
 				},
 			})
 		},
@@ -94,13 +191,80 @@ func init() {
 	})
 }
 
+func NewStudent(firstName string, lastName string, grade int, teacherName string, studentNumber int) *Student {
+	firstInitial := string(firstName[0])
+	lastInitial := string(lastName[0])
+	teacherInitial := string(teacherName[0])
+
+	student := new(Student)
+	student.Initials = [2]string{firstInitial, lastInitial}
+	student.Grade = grade
+	student.StudentNumber = studentNumber
+	student.TeacherInitial = teacherInitial
+
+	return student
+}
+
+func compareStudents(studentOne *Student, studentTwo *Student) bool {
+	if studentOne.Initials[0] == studentTwo.Initials[0] &&
+		studentOne.Initials[1] == studentTwo.Initials[1] &&
+		studentOne.Grade == studentTwo.Grade &&
+		studentOne.TeacherInitial == studentTwo.TeacherInitial {
+		return true
+	}
+	return false
+}
+
+func verifyStudent(student *Student, students *Students) (bool, error) {
+
+	firstInitialPosition := []rune(student.Initials[0])[0] - 65
+	if firstInitialPosition < 0 || firstInitialPosition > 26 {
+		return false, errors.New("first initial not an uppercase character")
+	}
+
+	lastInitialPosition := []rune(student.Initials[1])[0] - 65
+	if lastInitialPosition < 0 || lastInitialPosition > 26 {
+		return false, errors.New("last initial not an uppercase character")
+	}
+
+	initialsArr := students.Students[firstInitialPosition][lastInitialPosition]
+
+	for i := 0; i < len(initialsArr); i++ {
+		if compareStudents(student, &initialsArr[i]) {
+			return true, nil
+		}
+	}
+
+	return false, nil
+}
+
+var students *Students
+var guilds *Guilds
+
 func main() {
-	s.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
-		log.Println("Bot is up!")
-	})
 	err := s.Open()
 	if err != nil {
 		log.Fatalf("Cannot open the session: %v", err)
+	}
+
+	file, err := ioutil.ReadFile("students.json")
+	if err != nil {
+		log.Fatalf("Could not open students.json")
+	}
+
+	err = json.Unmarshal([]byte(file), &students)
+	if err != nil {
+		log.Fatalf("Could not unmarshal students.json")
+	}
+
+	file, err = ioutil.ReadFile("guilds.json")
+	if err != nil {
+		log.Fatalf("Could not open guilds.json")
+	}
+
+	err = json.Unmarshal([]byte(file), &guilds)
+	if err != nil {
+		log.Fatalf("Could not unmarshal guilds.json")
 	}
 
 	for _, v := range commands {
@@ -112,7 +276,7 @@ func main() {
 
 	defer s.Close()
 
-	stop := make(chan os.Signal)
+	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, os.Interrupt)
 	<-stop
 	log.Println("Gracefully shutdowning")
